79b8b585f8e08d8fe5e87ab1cd786bb64945a1ec
jcqlng <jcqlngeiger@outlook.com>
2020-05-02 17:56:33 -0400

    Created a start function click. Started the array of questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// The start button function to commence the game
+$("#start").on("click", function(){
+    $("#start").remove();
+})
+
+// Setting up the array of questions
+var questions = [{
+    question: "What does Mario jump on when he completes a level?",
+    answers: ["A tree", "A castle", "A flag pole", "Yoshi"],
+    correctAnswer: "A flag pole",
+    image: "assets/images/"
+
+}]
