558ac8590daa2e06b8382fa3521d61d2f1cd1aaf
jcqlng <jcqlngeiger@outlook.com>
2020-05-02 18:52:27 -0400

    added some psuedo code to get a layout of the code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -54,5 +54,39 @@ var questions = [{
     answers: ["A tree", "A castle", "A flag pole", "Yoshi"],
     correctAnswer: "A flag pole",
     image: "assets/images/img.gif"
+}]; 
+
+// Properties to the game
+var game= {
+    //the array of questions
+    questions:questions,
+    //current questions
+    currentQuestion:0,
+    //counts to 30
+    counter:30,
+    //shows right
+    correct:0,
+    //shows incorrect
+    incorrect:0,
+    //starts the countdown
+    countDown: function({
+
+    }),
+    // loads question
+
+    // shows next question
+    
+    // when the time is up
+
+    // shows result
+
+    // click function
+    
+    // when answered correctly
+
+    // when answered incorrectly 
+
+
+
+
 }
-]}
